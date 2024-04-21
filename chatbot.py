7eadfa4cea5751b4de07593d38f18cbf64394a18
Shivam Nimbalkar <91310677+Shiva008@users.noreply.github.com>
2024-04-21 15:16:01 +0530

    chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -49,17 +49,4 @@ if query:
         st.session_state.past.append(query)
         st.session_state.generated.append(response)
 
-# # Display conversation
-# if st.session_state['generated']:
-#     st.write("Conversation:")
-#     for i in range(len(st.session_state['generated'])):
-#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-#         message(st.session_state["generated"][i], key=str(i))
-if st.button("Download Conversation as JSON"):
-    conversation = {
-        "user_messages": st.session_state['past'],
-        "assistant_messages": st.session_state['generated']
-    }
-    with open("conversation.json", "w") as file:
-        json.dump(conversation, file)
-    st.success("Conversation downloaded as JSON file.")
+
